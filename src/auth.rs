61fcd667167c78cb08264272c787676c76583806
Martin Kim Dung-Pham <kim@elbedev.com>
2019-04-03 07:27:35 +0200

    Refactor Authenticator to take a client as parameter

--- src/auth.rs
+++ src/auth.rs
@@ -6,22 +6,30 @@ pub struct Authenticator {
 }
 
 impl Authenticator {
+    /// Convenience initialiser that returns an `Authenticator` with a default client
     pub fn new() -> Authenticator {
-        Authenticator {
-            client: reqwest::Client::new(),
-        }
+        Authenticator::authenticatorWithClient(reqwest::Client::new())
+    }
+
+    /// Creates an authenticator using the given client
+    fn authenticatorWithClient(client: reqwest::Client) -> Authenticator {
+        Authenticator { client }
     }
 
     /// Generate a session token for a given configuration
     ///
     /// ```
     /// use crate::paper::configuration::Configuration;
-    /// use crate::paper::auth;
+    /// use crate::paper::auth::Authenticator;
     /// let config = Configuration::new();
-    /// if let Ok(mut response) = auth::session_token_for_config(&config) {
-    ///     assert_eq!(response.text(), "abc".to_string());
+    /// let client = Authenticator::ReqwestClientMock::new();
+    /// let authenticator = Authenticator::authenticatorWithClient(client);
+    /// if let Ok(mut response) = authenticator.session_token_for_config(&config) {
+    ///     if let Ok(text) = response.text() {
+    ///         assert_eq!(text, "abc".to_string());
+    ///     }
     /// }
-    ///
+    /// ```
     pub fn session_token_for_config(&self, configuration: &Configuration) -> Result<Response> {
         let body = self.session_token_request_body(configuration.username, configuration.password);
         self.client
@@ -35,6 +43,15 @@ impl Authenticator {
             .send()
     }
 
+    /// Generates the session token request body for the given username and password
+    ///
+    /// ```
+    /// use crate::paper::configuration::Configuration;
+    /// use crate::paper::auth::Authenticator;
+    /// let authenticator = Authenticator::new();
+    /// let body = authenticator.session_token_request_body(&"abc", &"123");
+    /// assert_eq!(body.length(), 4);
+    /// ```
     fn session_token_request_body(&self, username: &str, password: &str) -> String {
         let x = format!(r#"<?xml version='1.0' encoding='utf-8'?>
         <soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'>
