4516f57a01d352e294b12c69d5be3c2ea3174bab
Martin Kim Dung-Pham <kim@elbedev.com>
2019-04-05 08:12:28 +0200

    Add xml lib

--- src/auth.rs
+++ src/auth.rs
@@ -1,3 +1,5 @@
+extern crate xml;
+
 use super::configuration::Configuration;
 use reqwest::{Response, Result};
 
@@ -52,7 +54,9 @@ impl Authenticator {
     /// let body = authenticator.session_token_request_body(&"abc", &"123");
     /// assert_eq!(body.length(), 4);
     /// ```
-    fn session_token_request_body(&self, username: &str, password: &str) -> String {
+    ///
+    /// public just for tests???
+    pub fn session_token_request_body(&self, username: &str, password: &str) -> String {
         let x = format!(r#"<?xml version='1.0' encoding='utf-8'?>
         <soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'>
             <soap12:Body>
