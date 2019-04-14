681bd68a2c9684ef936bdda4362e026f4928cbd6
Martin Kim Dung-Pham <kim@elbedev.com>
2019-04-14 15:18:14 +0200

    Cleanup session token parsing

--- src/xml/auth.rs
+++ src/xml/auth.rs
@@ -5,13 +5,6 @@ use xml::reader::{EventReader, XmlEvent};
 
 pub struct AuthXmlParser {}
 
-fn indent(size: usize) -> String {
-    const INDENT: &'static str = "    ";
-    (0..size)
-        .map(|_| INDENT)
-        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
-}
-
 impl AuthXmlParser {
     pub fn new() -> AuthXmlParser {
         AuthXmlParser {}
@@ -22,17 +15,12 @@ impl AuthXmlParser {
 
         let mut current_element = "".to_string();
         let parser = EventReader::new(xml);
-        let mut depth = 0;
         for e in parser {
             match e {
                 Ok(XmlEvent::StartElement {
                     name, attributes, ..
                 }) => {
                     current_element = name.to_string();
-                    depth += 1;
-                }
-                Ok(XmlEvent::EndElement { name }) => {
-                    depth -= 1;
                 }
                 Err(e) => {
                     println!("Error: {}", e);
