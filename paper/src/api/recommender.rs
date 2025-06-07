becc0ce63bd5a14622d21e4667efb80f904e1242
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2025-06-07 22:36:10 +0200

    refactor: update Recommender to use dynamic client and GPT-4o model

--- paper/src/api/recommender.rs
+++ paper/src/api/recommender.rs
@@ -1,26 +1,27 @@
+use crate::error::PaperError;
 use async_openai::{
     types::{ChatCompletionRequestMessage, CreateChatCompletionRequestArgs, Role},
     Client,
 };
-use crate::error::PaperError;
 use futures::future;
 
 #[derive(uniffi::Object)]
-pub struct Recommender {
-    client: Client,
-}
+pub struct Recommender {}
 
 #[uniffi::export]
 impl Recommender {
     #[uniffi::constructor]
     pub fn new() -> Self {
-        Self {
-            client: Client::new(),
-        }
+        Self {}
     }
 
-    pub async fn get_recommendations(&self, titles: Vec<String>, api_key: String) -> Result<Vec<String>, PaperError> {
-        let client = Client::with_config(async_openai::config::OpenAIConfig::new().with_api_key(api_key));
+    pub async fn get_recommendations(
+        &self,
+        titles: Vec<String>,
+        api_key: String,
+    ) -> Result<Vec<String>, PaperError> {
+        let client =
+            Client::with_config(async_openai::config::OpenAIConfig::new().with_api_key(api_key));
         let recommendations = future::join_all(titles.into_iter().map(|title| async {
             let messages = vec![
                 ChatCompletionRequestMessage {
@@ -31,19 +32,24 @@ impl Recommender {
                 },
                 ChatCompletionRequestMessage {
                     role: Role::User,
-                    content: format!("Suggest one similar book to '{}' and return just the title.", title),
+                    content: format!(
+                        "Suggest one similar book to '{}' and return just the title.",
+                        title
+                    ),
                     name: None,
                     function_call: None,
                 },
             ];
 
             let request = CreateChatCompletionRequestArgs::default()
-                .model("gpt-3.5-turbo")
+                .model("gpt-4o")
+                .store(false)
                 .messages(messages)
                 .max_tokens(50_u16)
-                .build()?;
+                .build()
+                .expect("do it");
 
-            match self.client.chat().create(request).await {
+            match client.chat().create(request).await {
                 Ok(response) => {
                     if let Some(choice) = response.choices.first() {
                         if let Some(content) = &choice.message.content {
@@ -55,7 +61,7 @@ impl Recommender {
                         Err(PaperError::GeneralError)
                     }
                 }
-                Err(_) => Err(PaperError::GeneralError)
+                Err(_) => Err(PaperError::GeneralError),
             }
         }))
         .await;
